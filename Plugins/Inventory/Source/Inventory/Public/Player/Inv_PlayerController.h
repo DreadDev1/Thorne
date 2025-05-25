efbb8f50e471b4256a87f9864956913b84956f17
DreadDev1 <kameen.avery@gmail.com>
2025-05-25 02:13:02 -0400

    Section 1 - Lecture 6 - Primary Interact Action

--- Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
+++ Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
@@ -6,13 +6,26 @@
 #include "GameFramework/PlayerController.h"
 #include "Inv_PlayerController.generated.h"
 
-/**
- * 
- */
+class UInputMappingContext;
+class UInputAction;
+
 UCLASS()
 class INVENTORY_API AInv_PlayerController : public APlayerController
 {
 	GENERATED_BODY()
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+
+	void PrimaryInteract();
+
+	// Create Input Mapping Context in Editor  <Assign to BP_Inv_PlayerController>
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TObjectPtr<UInputMappingContext> DefaultIMC;
+
+	// Create Input Action in Editor <Assign to BP_Inv_PlayerController>
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TObjectPtr<UInputAction> PrimaryInteractAction;
 };
